98db5f7510dd7d7b17301fac17dac47ff501d5ff
Kaung Htet San <kghtetsan26@gmail.com>
2025-01-15 11:11:03 +0630

    change typo and add extra input for fast apply job paragraph

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("applyButton").addEventListener("click", function () {
     console.log("Button clicked");
     // Get input values
     const managerName = document.getElementById("managerName").value;
+    const positionName = document.getElementById("positionName").value;
     const organizationName = document.getElementById("organizationName").value;
 
     console.log(managerName);
@@ -13,8 +14,9 @@ document.getElementById("applyButton").addEventListener("click", function () {
     let content = paragraph.innerHTML;
 
     // Replace placeholders with input values
-    content = content.replace("[...]", managerName);
-    content = content.replace("[....]", organizationName);
+    content = content.replace("[hiring manager]", managerName);
+    content = content.replace("[position]", positionName);
+    content = content.replace("[company]", organizationName);
 
     // Update the paragraph content
     paragraph.innerHTML = content;
